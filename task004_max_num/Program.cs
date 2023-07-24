db8fec72ae9944989a947eae83fb7290929ebc9f
Sofia <rybina.sofia@gmail.com>
2023-07-24 13:35:50 +0300

    Максимальное из 3х чисел

---
+++ task004_max_num/Program.cs
@@ -0,0 +1,35 @@
+﻿// Задача 4: Напишите программу, которая принимает на вход три числа и выдаёт максимальное из этих чисел.
+// 2, 3, 7 -> 7
+// 44 5 78 -> 78
+// 22 3 9 -> 22
+
+
+Console.WriteLine("Hello, write the first number:");
+int num1 = int.Parse(Console.ReadLine()!);
+Console.WriteLine("Hello, write the second number:");
+int num2 = int.Parse(Console.ReadLine()!);
+Console.WriteLine("Hello, write the third number:");
+int num3 = int.Parse(Console.ReadLine()!);
+Console.WriteLine();
+
+int max = 0;
+
+if(num1 > num2 && num1 > num3)
+{
+    max = num1;
+    Console.WriteLine($"{num1}, {num2}, {num3} -> {max}");
+}
+else if(num2 > num1 && num2 > num3)
+{
+    max = num2;
+    Console.WriteLine($"{num1}, {num2}, {num3} -> {max}");
+}
+else if(num3 > num1 && num3 > num2)
+{
+    max = num3;
+    Console.WriteLine($"{num1}, {num2}, {num3} -> {max}");
+}
+else
+{
+    Console.WriteLine($"{num1}, {num2}, {num3} -> The numbers are equal");    
+}
